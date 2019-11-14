ab57ffcf760c310953cdecb0df796b7711156fed
Melissendra <melissendra77@gmail.com>
2019-11-14 14:29:22 +0100

    part one: change of pages functions ok

--- js/Game.js
+++ js/Game.js
@@ -9,27 +9,37 @@ class Game {
         this.activePhrase = null;
     }
 
-/**
- * Creates phrases for use in game
- * @return {array} An array of phrases that could be used in the game
- * */
+    /**
+     * Creates phrases for use in game
+     * @return {array} An array of phrases that could be used in the game
+     * */
     createPhrases(){
-        const phrases = [
+        return [
             new Phrase("Carpe Diem"),
             new Phrase("Treat others like you'd like to be treated"),
             new Phrase("We often need a smaller one than ourselves"),
             new Phrase("The eyes are the soul's mirror"),
             new Phrase("Never say never")
         ];
-        return phrases;
     }
 
-/**
- * Selects random phrase from phrases property
- * @return {object} Phrase object chosen to be used
- * */
+    /**
+     * Selects random phrase from phrases property
+     * @return {object} Phrase object chosen to be used
+     * */
     getRandomPhrase(){
         const randomNumber = Math.floor(Math.random() * this.phrases.length);
         return this.phrases[randomNumber];
+    }
+
+    /**
+     * begins game by selecting a random phrase and displaying it to the user
+     **/
+    startGame(){
+        const $overlayDiv = $("#overlay");
+        $overlayDiv.hide();
+        const random = this.getRandomPhrase();
+        random.addPhraseToDisplay();
+        this.activePhrase = random;
     }
 }
--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const game = new Game();
-const phrase = new Phrase();
+const $button = $("#btn__reset");
+let game;
+
+$button.on("click", () =>{
+   game = new Game();
+   game.startGame();
+});
+
+
+
